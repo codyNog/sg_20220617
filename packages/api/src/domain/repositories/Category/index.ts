2e537f400d4691c91fd1a3f715c32213d5261d9d
codyNog <yoneyoneyone99@gmail.com>
2022-06-17 17:36:51 +0900

    categoryのCRUD

--- packages/api/src/domain/repositories/Category/index.ts
+++ packages/api/src/domain/repositories/Category/index.ts
@@ -2,15 +2,34 @@ import { Category } from "@my/shared/entities/Category";
 import { prisma } from "~/libs/prisma";
 import { GetCategoriesQuery } from "@my/shared/api/Category/queries";
 
-const create = async (category: Category): Promise<Category> => {};
+const create = async (category: Category): Promise<Category> => {
+  return await prisma.category.create({ data: category });
+};
 
-const getMany = async (param: GetCategoriesQuery): Promise<Category[]> => {};
+const getMany = async (param: GetCategoriesQuery): Promise<Category[]> => {
+  return await prisma.category.findMany({ where: param });
+};
 
-const get = async (uid: string): Promise<Category> => {};
+const get = async (uid: string): Promise<Category> => {
+  const category = await prisma.category.findUnique({ where: { uid } });
 
-const update = async (category: Category): Promise<Category> => {};
+  if (!category) throw 404;
 
-const deleteCategory = async (uid: string): Promise<void> => {};
+  return category;
+};
+
+const update = async (category: Category): Promise<Category> => {
+  const newCategory = await prisma.category.update({
+    data: category,
+    where: { uid: category.uid },
+  });
+
+  return newCategory;
+};
+
+const deleteCategory = async (uid: string): Promise<void> => {
+  await prisma.category.delete({ where: { uid } });
+};
 
 export const CategoryImpl = {
   create,
